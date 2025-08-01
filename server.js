e46f69f0bd1b8d8282cc5be4eeb4bb7c2f30df01
dieginsa <dieginsa1@gmail.com>
2025-08-01 09:58:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static(__dirname));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000, () => {
